fc1b6495c861e5e723b8d8f47e3e1d4a8dcd0a4d
M.Daenjel <daenjelkirigo@gmail.com>
2019-03-09 16:54:48 +0300

    Update airtime.py
    clear credentials

--- controllers/airtime.py
+++ controllers/airtime.py
@@ -2,8 +2,8 @@
 import africastalking
 
 #Define credentials here
-username = "sandbox"
-api_key = "fbba33f71f5f671f7b1043ff721eec2140c29d6d4e212c23f9c4edcec6b6b898"
+username = "sandbox NAME"
+api_key = "YOUR API KEY"
 
 #Authenticate with the service
 africastalking.initialize(username, api_key)
@@ -12,7 +12,7 @@ africastalking.initialize(username, api_key)
 airtime = africastalking.Airtime 
 
 #Define user variables
-phone_number = "+254733745544"
+phone_number = "YOUR PHONE NUMBER"
 amount = "300"
 currency_code = "KES"
 
